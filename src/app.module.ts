65e5b8cf4f08f516d570363b49f08812c5bd5c7c
Victor Lima Vieira <victorlvminhaconta@gmail.com>
2026-02-23 11:15:33 -0300

    feat: configura o banco na nuvem e variaveis de ambiente

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm'; // Importando o módulo do TypeORM
+import { ConfigModule } from '@nestjs/config'; // 🚨 1. Importando o leitor do .env
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsuariosModule } from './usuarios/usuarios.module';
@@ -9,17 +10,20 @@ import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
+    // 🚨 2. Ligando o cofre logo no início
+    ConfigModule.forRoot(),
+
     // Configuração do Banco de Dados
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'postgres',    
-      database: 'nest_db',  // Nome do banco
-      entities: [],         // Aqui vão entrar as tabelas (Entidades)
-      synchronize: true,    // 🚨 Cria as tabelas sozinho Só usar em DEV!
+      // 🚨 3. Trocamos as 5 linhas locais por uma única linha que lê a URL da nuvem!
+      url: process.env.DATABASE_URL, 
+      entities: [],          // Aqui vão entrar as tabelas (Entidades)
+      synchronize: true,     // 🚨 Cria as tabelas sozinho Só usar em DEV!
       autoLoadEntities: true,
+      ssl: {
+        rejectUnauthorized: false, // 🚨 4. Exigência obrigatória da Render para bancos na nuvem!
+      },
     }),
     UsuariosModule,
     MedicamentosModule,
